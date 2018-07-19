d0097426a3fa6993468c02e7db7be5498c82787d
d-smith <doug.smith.mail@gmail.com>
2018-07-19 13:30:29 -0700

    Updates to invoke an endpoint from a lambda

--- handler.js
+++ handler.js
@@ -1,14 +1,28 @@
-module.exports.hello = function(event, context, callback) {
+const rp = require('request-promise-native');
 
-    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        "x-custom-header" : "My Header Value"
-      },
-      body: JSON.stringify({ "message": "Hello World!" })
+const getUrl = async (callback) => {
+    let options = {
+        method: 'GET',
+        uri: process.env.ENDPOINT
     };
 
-    callback(null, response);
+    try {
+        let callResult = await rp(options);
+        console.log(callResult);
+        const response = {
+            statusCode: 200,
+             body: callResult
+        };
+        callback(null, response);
+    } catch(theError) {
+        const response = {
+            statusCode: 500,
+             body: 'ding it'
+        };
+    }
+}
+
+module.exports.hello = async (event, context, callback) => {
+    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+    await getUrl(callback);
 };
